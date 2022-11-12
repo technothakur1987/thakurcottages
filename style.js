02bb240fafa2f78f2b893f6baec45c6a9ae4cd69
technothakur1987 <technothakur1987@gmail.com>
2022-11-12 07:16:32 +0530

    commit

---
+++ style.js
@@ -0,0 +1,36 @@
+console.log("Welcome to thakurHotels");
+
+
+
+
+
+
+    function hover1(){
+        console.log("hover1 function is activated");
+        document.querySelector(".content1").style.display = "flex";
+        
+    ;}
+    function hover1close(){
+        document.querySelector(".content1").style.display = "none";
+    
+    }
+    function hover2(){
+        console.log("hover2 function is activated");
+        document.querySelector(".content2").style.display = "flex";
+        
+    ;}
+    function hover2close(){
+        document.querySelector(".content2").style.display = "none";
+    
+    }
+    function hover3(){
+        console.log("hover3 function is activated");
+        document.querySelector(".content3").style.display = "flex";
+        
+    ;}
+    function hover3close(){
+        document.querySelector(".content3").style.display = "none";
+    
+    }
+    
+
